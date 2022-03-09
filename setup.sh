bbfa42b6a13b311ad59d3434c80052bd9dcef674
Sailesh <sailesh@netspring.io>
2022-03-09 11:25:27 +0000

    Fixes to setup.

--- setup.sh
+++ setup.sh
@@ -2,11 +2,14 @@
 
 set -eo pipefail
 
+# Vimified is setup from hom dir.
+pushd ~
 echo "Setup Vimified"
 curl -L https://raw.github.com/saileshmittal/vimified/master/install.sh | sh
+popd
 
 # Copy dotfiles to home dir
-cp dotfiles/* ~/
+cp -R dotfiles/. ~/
 
 # Source the local bashrc
 cat <<EOF >> ~/.bashrc
